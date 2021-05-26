1bcd51b496cd05bcad43f950fb63fdc63a5ad75f
Sonika <sonikapoudyal5@gmail.com>
2021-05-25 21:56:06 -0400

    created function for display wea†her, date and icon

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -6,7 +6,7 @@ var cityFormEl = document.querySelector("#city-search-form");
 var formInputEl = document.querySelector("#form-input");
 var formSubmitEl = document.querySelector("#btn");
 var forecastEl = document.querySelector("#forecast");
-var citySearchEl = document.querySelector("#searched-city");
+var createFiveDayEl = document.querySelector("#five-day-forecast");
 
 
 // function to operate the search button 
@@ -16,59 +16,97 @@ var formSubmitHandler = function (event) {
     var city = formInputEl.value;
 
     if (city) {
-        // getUserRepos(city);
         formInputEl.value = "";
     } else {
         alert("please enter a name of a city");
     }
-    console.log(city);
 
-    saveSearchCities();
+    getWeather(city);
 };
 
 //function to save the searched cities in the local storage
-var saveSearchCities = function () {
-    localStorage.setItem("cities", JSON.stringify(cities));
+var saveSearchCities = function (city) {
+    var savedCities = JSON.parse(localStorage.getItem("cities"));
+    if (savedCities === null) {
+        var array = [city]
+        localStorage.setItem("cities", JSON.stringify(array));
+    } else {
+        savedCities.push(city);
+        localStorage.setItem("cities", JSON.stringify(savedCities));
+    }
 };
+
 
 //function to fetch the apiUrl 
 
 var getWeather = function (city) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={384fed659b9ff3e7bd8ebc325dacf9b8}'
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=384fed659b9ff3e7bd8ebc325dacf9b8`
+    
     fetch(apiUrl)
-    .then(function (response){
-        response.json().then(function (data) {
-             console.log(data);
-            displayWeather(data, city);
+        .then(function (response) {
+            response.json().then(function (data) {
+                console.log(data);
+                displayWeather(data);
+                saveSearchCities(data.name);
+            });
         });
-    });
-
 };
 
 // function to displayWeather
 
 var displayWeather = function (weather) {
-forecastEl.textContent="";
-// console.log(weather);
+    forecastEl.textContent = "";
+    var cityName = document.createElement("h3");
+    cityName.textContent = weather.name
+    forecastEl.append(cityName);
+    // console.log(weather);
 
+    //function to create a date next to the city
+    var currentDate = document.createElement("h5")
+    currentDate.textContent = moment().format("MMMM Do YYYY");
+    forecastEl.appendChild(currentDate);
 
-//function to create a date next to the city
-var currentDate = document.createElement()
-currentDate.textContent = moment().format("MMMM Do YYYY");
-citySearchEl.appendChild(currentDate);
+    //function to put an icon next to the date
+    var weatherIcon = document.createElement("img")
+    weatherIcon.setAttribute("src", `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+    forecastEl.append(weatherIcon);
 
-//function to put an icon next to the date
+    //function to create a temperature data
 
-//function to create a temperature data
+    //function to create the humidity data
 
-//function to create the humidity data
+    //function to create wind speed
 
-//function to create the UV index
-
+    //function to create the UV index
+    var URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${weather.coord.lat}&lon=${weather.coord.lon}&appid=384fed659b9ff3e7bd8ebc325dacf9b8`
+    fetch(URL)
+        .then(function (response) {
+            response.json().then(function (weather) {
+                
+                console.log(weather);
+            
+            });
+        });
 
 };
 
 //function to create 5 day forecast 
+
+// var get5Day = function (city) {
+//     var apiUrl = '';
+//     fetch(apiUrl)
+//         .then(function (response) {
+//             response.json().then(function (data) {
+//                 console.log(data);
+//                 display5Day(data);
+//             });
+//         });
+// }
+
+// var display5Day = function (weather) {
+// createFiveDayEl.textContent = ""
+
+// }
 
 
 
