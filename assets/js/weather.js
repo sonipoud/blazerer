fd51ec5790e81e31f8e1c63065c90801c09eaef8
Sonika <sonikapoudyal5@gmail.com>
2021-05-25 23:18:54 -0400

    added the uv index function

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -41,7 +41,7 @@ var saveSearchCities = function (city) {
 
 var getWeather = function (city) {
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=384fed659b9ff3e7bd8ebc325dacf9b8`
-    
+
     fetch(apiUrl)
         .then(function (response) {
             response.json().then(function (data) {
@@ -59,12 +59,11 @@ var displayWeather = function (weather) {
     var cityName = document.createElement("h3");
     cityName.textContent = weather.name
     forecastEl.append(cityName);
-    // console.log(weather);
 
     //function to create a date next to the city
-    var currentDate = document.createElement("h5")
-    currentDate.textContent = moment().format("MMMM Do YYYY");
-    forecastEl.appendChild(currentDate);
+    var currentDate = document.createElement("span")
+    currentDate.textContent = "(" + moment().format("MMMM Do, YYYY") + ")";
+    forecastEl.append(currentDate);
 
     //function to put an icon next to the date
     var weatherIcon = document.createElement("img")
@@ -72,36 +71,64 @@ var displayWeather = function (weather) {
     forecastEl.append(weatherIcon);
 
     //function to create a temperature data
+    var temperature = document.createElement("span")
+    temperature.textContent = "Temperature: " + weather.main.temp + "°F";
+    temperature.classList = "list-group-item"
+    forecastEl.append(temperature);
 
     //function to create the humidity data
+    var humidity = document.createElement("span")
+    humidity.textContent = "Humidity:" + weather.main.humidity + "%";
+    humidity.classList = "list-group-item"
+    forecastEl.append(humidity);
 
     //function to create wind speed
+    var windSpeed = document.createElement("span")
+    windSpeed.textContent = "Wind Speed:" + weather.wind.speed + "MPH"
+    windSpeed.classList = "list-group-item"
+    forecastEl.append(windSpeed);
 
-    //function to create the UV index
+    uvIndex(weather);
+
+};
+
+//function to create the UV Index
+var uvIndex = function (weather) {
     var URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${weather.coord.lat}&lon=${weather.coord.lon}&appid=384fed659b9ff3e7bd8ebc325dacf9b8`
     fetch(URL)
         .then(function (response) {
             response.json().then(function (weather) {
-                
                 console.log(weather);
-            
+                displayUvIndex(weather);
             });
         });
+}
 
-};
+// function to display UV Index
+var displayUvIndex = function (index) {
+    var uvIndex = document.createElement("div");
+    uvIndex.textContent = "UV Index:"
+    // + weather.current.uvi
+    uvIndex.classList = "List-group-item"
+
+    uvIndexValue = document.createElement("span")
+    uvIndexValue.textContent = index.value
+
+    uvIndex.append(uvIndexValue);
+    forecastEl.append(uvIndex);
+}
 
 //function to create 5 day forecast 
-
-// var get5Day = function (city) {
-//     var apiUrl = '';
-//     fetch(apiUrl)
-//         .then(function (response) {
-//             response.json().then(function (data) {
-//                 console.log(data);
-//                 display5Day(data);
-//             });
-//         });
-// }
+var get5Day = function (city) {
+    var api = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=384fed659b9ff3e7bd8ebc325dacf9b8`;
+    fetch(api)
+        .then(function (response) {
+            response.json().then(function (data) {
+                console.log(data);
+                // display5Day(data);
+            });
+        });
+}
 
 // var display5Day = function (weather) {
 // createFiveDayEl.textContent = ""
