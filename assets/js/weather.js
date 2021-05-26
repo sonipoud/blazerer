43d876d8b668ff5f6b26c88515fc270e1a329700
Sonika <sonikapoudyal5@gmail.com>
2021-05-26 19:04:29 -0400

    got the object working for uv index

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -97,7 +97,8 @@ var uvIndex = function (weather) {
     var URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${weather.coord.lat}&lon=${weather.coord.lon}&appid=384fed659b9ff3e7bd8ebc325dacf9b8`
     fetch(URL)
         .then(function (response) {
-            response.json().then(function (weather) {
+            response.json()
+            .then(function (weather) {
                 console.log(weather);
                 displayUvIndex(weather);
             });
@@ -118,24 +119,69 @@ var displayUvIndex = function (index) {
     forecastEl.append(uvIndex);
 }
 
-//function to create 5 day forecast 
+// //function to create 5 day forecast 
 var get5Day = function (city) {
-    var api = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=384fed659b9ff3e7bd8ebc325dacf9b8`;
+    var api = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=384fed659b9ff3e7bd8ebc325dacf9b8`
     fetch(api)
         .then(function (response) {
-            response.json().then(function (data) {
-                console.log(data);
-                // display5Day(data);
-            });
+            return response.json()
+        })
+        .then(function (weather) {
+            console.log(weather);
+            // display5Day(data);
         });
+
 }
 
+get5Day("denver");
+
+
 // var display5Day = function (weather) {
-// createFiveDayEl.textContent = ""
+//     createFiveDayEl.textContent = ""
+//     var fiveDay = document.createElement("h5")
+//     fiveDay.textContent = "5-Day Forecast:";
+//     console.log(weather);
+//     createFiveDayEl.append(fiveDay);
 
-// }
+//     var forecast = weather.list;
+//     for (var i = 5; i < forecast.length; i = i + 8) {
+//         var dailyForecast = forecast[i];
+
+//         var createFiveDayEl = document.createElement("div");
+//         createFiveDayEl.classList = "card bg-primary text-ligh m-2";
 
 
+//         //function to create a date next to the city
+//         var forecastDate = document.createElement("h6")
+//         forecastDate.textContent = "(" + moment().format("MMMM Do, YYYY") + ")";
+//         forecastDate.classList = "card-header text-center"
+//         createFiveDayEl.append(forecastDate);
 
+//         //function to put an icon next to the date
+//         var forecastWeatherIcon = document.createElement("img")
+//         forecastWeatherIcon.setAttribute("src", `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+//         forecastWeatherIcon.classList = "card-header text-center"
+//         createFiveDayEl.append(forecastWeatherIcon);
+
+
+//         //function to create a temperature data
+//         var forecastTemperature = document.createElement("span")
+//         forecastTemperature.textContent = "Temperature: " + weather.main.temp + "°F";
+//         forecastTemperature.classList = "card-header text-center"
+//         createFiveDayEl.append(forecastTemperature);
+
+//         //function to create the humidity data
+//         var forecastHumidity = document.createElement("span")
+//         forecastHumidity.textContent = "Humidity:" + weather.main.humidity + "%";
+//         forecastHumidity.classList = "card-header text-center"
+//         createFiveDayEl.append(forecastHumidity);
+
+//         //function to create wind speed
+//         var forecastWindSpeed = document.createElement("span")
+//         forecastWindSpeed.textContent = "Wind Speed:" + weather.wind.speed + "MPH"
+//         forecastWindSpeed.classList = "card-header text-center"
+//         createFiveDayEl.append(forecastWindSpeed);
+//     };
+// };
 
 formSubmitEl.addEventListener("click", formSubmitHandler);
